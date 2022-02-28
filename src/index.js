01fbf9e5a3bc62cdf5eb7df4e7c6cba89b0a504f
Drew Lacob <dlacob14@gmail.com>
2022-02-28 13:00:18 -0800

    manual merge of workout page, fix so login page displays
    merged in workout page and components. also brought in Nick's work on routers but couldn't get it working so left login page to display. Commented out other imports to reduce warnings

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import reportWebVitals from './reportWebVitals';
+//import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
