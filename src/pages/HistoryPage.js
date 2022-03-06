f91326e9bb77479c1769e5b4037b9f6a245b2cf7
Spencer-Stice <72483526+Spencer-Stice@users.noreply.github.com>
2022-03-06 13:47:36 -0800

    Manually update the history page for comms

--- src/pages/HistoryPage.js
+++ src/pages/HistoryPage.js
@@ -17,42 +17,61 @@ import FolderIcon from '@mui/icons-material/Folder';
 import DeleteIcon from '@mui/icons-material/Delete';
 import PlayCircleFilledWhiteIcon from '@mui/icons-material/PlayCircleFilledWhite';
 import Button from '@mui/material/Button';
-
+import NavBar from '../components/NavBar.js'
+import { textAlign } from '@material-ui/system';
+var result = "";
 
 export default function HistoryPage() {
+  const axios = require('axios');
+
+// Make a request for a user with a given ID
+axios.get('http://localhost:8888/allHist?user=1234')
+  .then(function (response) {
+    // handle success
+    result = response;
+    console.log(response)
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+
+
     function generate(element) {
-        return [0, 1, 2].map((value) =>
-          React.cloneElement(element, {
-            key: value,
-          }),
-        );
+      return [0, 1, 2, 3, 4, 5].map((value) =>
+        React.cloneElement(element, {
+          key: value,
+        }),
+      );
       }
-      
+    
 
-  const [dense, setDense] = React.useState(false);
-  const [secondary, setSecondary] = React.useState(true);
+const [dense, setDense] = React.useState(false);
+const [secondary, setSecondary] = React.useState(true);
 
-  return (
-        <React.Fragment>
-            <div>
-                <h1>Hello</h1>
-            </div>
-            <List dense={dense} sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.blue' }}>
-              {generate(
-                <ListItem
-                  secondaryAction={
-                    <Button variant="contained" endIcon={<PlayCircleFilledWhiteIcon />}>
-                    Start
-                    </Button>
-                  }
-                >
-                  <ListItemText
-                    primary="Workout Number X"
-                    secondary={secondary ? 'Date: X' : null}
-                  />
-                </ListItem>,
-              )}
-            </List>
-        </React.Fragment>
-  );
-}
+return (
+  <Box sx={{ flexGrow: 1}}>
+    <NavBar></NavBar>
+          <List dense={dense} sx={{ width: '200%', maxWidth: 360, bgcolor: 'background.blue'}}>
+            {generate(
+              <ListItem
+                secondaryAction={
+                  <Button variant="contained" endIcon={<PlayCircleFilledWhiteIcon />}>
+                  Start
+                  </Button>
+                }
+              >
+                <ListItemText
+                  primary= {result}
+                  secondary={secondary ? 'Date: X' : null}
+                />
+              </ListItem>,
+            )}
+          </List>
+  </Box>
+);
+
+  }
