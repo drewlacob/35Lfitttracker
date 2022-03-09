db55e63833578cbff96e8687b59884e4c2572700
Spencer-Stice <72483526+Spencer-Stice@users.noreply.github.com>
2022-03-08 23:31:33 -0800

    Adding App.js

--- src/App.js
+++ src/App.js
@@ -2,25 +2,97 @@ import React, { Component } from 'react'
 import './App.css';
 import LoginPage from './pages/LoginPage.js'
 import WorkoutPage from './pages/WorkoutPage.js';
+import HistoryPage from './pages/HistoryPage.js'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+window.result = ""
+var UID = "";
+let name = sessionStorage.getItem("user_name")
+console.log("UID: ")
+console.log(UID)
+console.log("name: ")
+console.log(name)
+window.fromhist = 0;
 //import { Switch, Link, Redirect } from 'react-router-dom';
+const axios = require('axios');
+function renderMyData(){
+axios.get('http://localhost:8888/allHist?user=' + UID)
+          .then(function (response) {
+    // handle success
+             window.result = response.data;
+             console.log("Just got it")
+             console.log(window.result)
+           })
+           .catch(function (error) {
+    // handle error
+             console.log(error);
+          })
+          .then(function () {
+        // always executed
+         });
+        }
+
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <LoginPage></LoginPage>
-          <Router>
-            <Routes>
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/workouts" element={<WorkoutPage />} />
-            </Routes>
-          </Router>
-        </header> 
-      </div>
-    )
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      result : null,
+      done : false
+    };
   }
 
+  componentDidMount() {
+
+   this.renderMyData();
+   console.log("running set");
+   setTimeout(() => {
+    this.setState((state) =>
+    { 
+     return {result : window.result, done : true}
+     
+   })
+   }, 100);
 }
 
+
+
+renderMyData(){
+  console.log("running comp")
+  UID = sessionStorage.getItem("user_id")
+  axios.get('http://localhost:8888/allHist?user=' + UID)
+            .then(function (response) {
+      // handle success
+               window.result = response.data;
+               console.log("Just got it");
+               console.log(window.result);
+             })
+             .catch(function (error) {
+      // handle error
+               console.log(error);
+            })
+            .then(function () {
+          // always executed
+           });
+          }
+
+    render() {
+      console.log("render ran")
+      console.log(window.result)
+      return (
+        <div className="App">
+          <header className="App-header">
+            <Router>
+              <Routes>
+                <Route path="" element={<LoginPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/workouts" element={<WorkoutPage />} />
+                <Route path="/history" element={this.state.done ? <HistoryPage /> : <div> LOADING </div>} />
+              </Routes>
+            </Router>
+          </header> 
+        </div>
+      )
+    }
+  }
 export default App;
